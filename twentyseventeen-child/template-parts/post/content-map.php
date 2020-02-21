1f42a5bd4755b0e577657e7892c6b1776800b590
Bruce Hoppe <32456695+beh333@users.noreply.github.com>
2020-02-21 13:28:38 -0500

    Final 2019 production
    Starting point for 2020

--- twentyseventeen-child/template-parts/post/content-map.php
+++ twentyseventeen-child/template-parts/post/content-map.php
@@ -10,24 +10,29 @@
  * @version 1.2
  */
 
-// display map marker for exhibit as well
-//
-
 $post_type = get_post_type();
 if ('porch' == $post_type) {
     $location = get_field('map_marker');
+    $marker_label = get_field('marker_label');
 } elseif (('band' == $post_type) || ('exhibit' == $post_type)) {
     $host_id = get_field('porch_link');
-    $location = get_field('map_marker', $host_id);
-    if (!$location) {
+    if ($host_id) {
+        $location = get_field('map_marker', $host_id);
+        $marker_label = get_field('marker_label', $host_id);
+        if (!$location) {
+            return;
+        }
+    } else {
         return;
     }
 } else {
     return;
 }
-
+if ($marker_label == '9999') {
+    $marker_label = '';
+}
 ?>
-<div class="marker" data-lat="<?php echo $location['lat']; ?>"
+<div class="marker" data-label="<?php echo $marker_label; ?>" data-lat="<?php echo $location['lat']; ?>"
 		data-lng="<?php echo $location['lng']; ?>">
 	<?php get_template_part( 'template-parts/post/content', 'excerpt' ); ?>
 </div>
